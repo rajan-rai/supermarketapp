de3988fc252a2fc56ba8131c9c267bda01dce5ba
rajan-rai <rajan.rai@hotmail.com>
2020-08-11 17:45:15 +0530

    Refresh Issue

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,14 +3,14 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAJ1ofJfZcu0q9J7DAmQlNCNghqsh-_vWw",
-    authDomain: "firstnativeapp-bfe62.firebaseapp.com",
-    databaseURL: "https://firstnativeapp-bfe62.firebaseio.com",
-    projectId: "firstnativeapp-bfe62",
-    storageBucket: "firstnativeapp-bfe62.appspot.com",
-    messagingSenderId: "654220715685",
-    appId: "1:654220715685:web:525869896446f59aea4f4c",
-    measurementId: "G-PPYW6ZPX8Y"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
   const app = firebase.initializeApp(firebaseConfig);
